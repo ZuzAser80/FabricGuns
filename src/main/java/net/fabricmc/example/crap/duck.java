8ea844a8468969005936d7eea5c7acdb681ed99e
ZuzAser80 <dde4th@gmail.com>
2022-01-26 22:44:15 +0500

    fds

--- src/main/java/net/fabricmc/example/crap/duck.java
+++ src/main/java/net/fabricmc/example/crap/duck.java
@@ -1,14 +1,11 @@
 package net.fabricmc.example.crap;
 
-
-import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-public interface MatrixAccess {
+public interface duck {
     void methodAccess(MatrixStack matrices, VertexConsumer vertices, int light, int overlay);
-    ModelPart getPlayerHand();
+    void getHand(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity player);
 }
--- src/main/java/net/fabricmc/example/mixin/ModelPartAccessMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ModelPartAccessMixin.java
@@ -1,17 +1,17 @@
 package net.fabricmc.example.mixin;
 
-import net.fabricmc.example.crap.MatrixAccess;
+import net.fabricmc.example.crap.duck;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ModelPart.class)
-public abstract class ModelPartAccessMixin implements MatrixAccess {
+public abstract class ModelPartAccessMixin implements duck {
 
     @Shadow public abstract void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha);
+
     @Override
     public void methodAccess(MatrixStack matrices, VertexConsumer vertices, int light, int overlay)
     {
